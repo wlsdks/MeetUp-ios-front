53e8dd10569df35c27e62cf598604f1ca3f37a28
jinan <ja.choi@sweetk.co.kr>
2023-06-02 22:52:02 +0900

    1. 주석추가 2. package 추가

--- MeetUp-ios-front/ViewController.swift
+++ MeetUp-ios-front/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - 뷰 실행시 맨 처음 동작한다.
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+// MARK: - 메서드를 추가할 확장 선언
+private extension ViewController {
+    
 }
 
